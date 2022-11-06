563ee26c4c378103241d968b66889b281ba1b0b4
James Bachini <james@mcambridge.com>
2022-11-06 15:17:07 +0000

    sort from and to transactions

--- view.js
+++ view.js
@@ -10,16 +10,29 @@ const alchemy = new Alchemy(settings);
 let address = process.argv[2];
 
 const lookup = async () => {
-    const transfers = await alchemy.core.getAssetTransfers({
-        fromBlock: "0x0",
-        toBlock: "latest",
-        fromAddress: address,
-        category:  ["external", "internal", "erc20"]
-      });
-    //console.log(transfers);
-    transfers.transfers.forEach((t) => {
-        console.log(`${t.category},${t.asset},${t.value},${t.from},${t.to},https://goerli.etherscan.io/tx/${t.hash}`);
-    });
+	const fromTransactions = await alchemy.core.getAssetTransfers({
+		fromBlock: "0x0",
+		toBlock: "latest",
+		fromAddress: address,
+		category:  ["external", "internal", "erc20"]
+	});
+	const toTransactions = await alchemy.core.getAssetTransfers({
+		fromBlock: "0x0",
+		toBlock: "latest",
+		toAddress: address,
+		category:  ["external", "erc20"]
+	});
+	const cleaned = [];
+	fromTransactions.transfers.forEach((t) => {
+		cleaned.push(t);
+	});
+	toTransactions.transfers.forEach((t) => {
+		cleaned.push(t);
+	});
+	cleaned.sort((a,b) => a.blockNum - b.blockNum);
+	cleaned.forEach((t) => {
+		console.log(`${t.category},${t.asset},${t.value},${t.from},${t.to},https://goerli.etherscan.io/tx/${t.hash}`);
+	});
 }
 
 lookup();
